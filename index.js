a165db8d3e67257609ad27270b27eba790bd2cbb
Sec4life.$ao <de7ket.3yonk.a@hotmail.com>
2023-12-13 15:07:01 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const taskRoutes = require('./routes/taskRoutes');
+const userRoutes = require('./routes/userRoutes');
+const authMiddleware = require('./middleware/authMiddleware');
+
+const app = express();
+app.use(bodyParser.json());
+
+// Connect to MongoDB using mongoose
+mongoose.connect('mongodb://localhost:27017/taskDB', { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Routes
+app.use('/user', userRoutes); // User routes for registration, login, etc.
+app.use('/tasks', authMiddleware.verifyToken, taskRoutes); // Task routes requiring authentication
+
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
